dba5a6d7ef2c184775b5ba548ee6ebec37989b6b
Prerak Shah <prerak9350@gmail.com>
2024-10-02 00:59:58 +0930

    Version 1

---
+++ home/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Client
+
+
+def index(request):
+    clientName = Client.objects.all()
+    return render(request, 'index.html', {'name': clientName})
+
+
+def aboutUs(request):
+    return render(request, 'about-us.html')
+
